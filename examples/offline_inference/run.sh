7b22a8862e88d02768d3f9f67254f5fd3208841d
dbyoung18 <yang5.yang@intel.com>
2025-03-10 07:07:39 +0000

    refactor(deepseek): add flag NUM_DUMMY_LAYERS to modeling
    Signed-off-by: dbyoung18 <yang5.yang@intel.com>

--- examples/offline_inference/run.sh
+++ examples/offline_inference/run.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
+: ${MODEL=${1:-"deepseek-ai/DeepSeek-V3"}} # HF model id
+
 export TORCH_DEVICE_BACKEND_AUTOLOAD=0
 export VLLM_USE_V1=0
 export VLLM_MLA_DISABLE=1
+export NUM_DUMMY_LAYERS=4 # 3 dense + 1 moe
 
 # python cli.py \
 python llm_engine_example.py \
-    --model deepseek-ai/DeepSeek-V3 \
-    --trust-remote-code \
-    --enforce-eager \
-    --max-num-batched-tokens 256 \
-    --max-model-len 256
+  --model ${MODEL} \
+  --trust-remote-code \
+  --enforce-eager \
+  --max-num-batched-tokens 256 \
+  --max-model-len 256
